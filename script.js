5fed95fc9ab03dfbbc8dc69f808e171ae7a66a1a
kalalpavangoud01-byte <kalalpavangoud01@gmail.com>
2026-09-19 16:28:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ const inputs = document.querySelectorAll('.controls input');
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+    const video = document.querySelector('video');
+const playerButton = document.querySelector('.player__button');
+const progressFilled = document.querySelector('.progress__filled');
+
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+        playerButton.textContent = '❚❚';
+    } else {
+        video.pause();
+        playerButton.textContent = '►';
+    }
+}
+
+playerButton.addEventListener('click', togglePlay);
+
+video.addEventListener('timeupdate', () => {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressFilled.style.flexBasis = `${percent}%`;
+});
+
+const rewind = document.querySelector('[data-skip="-10"]');
+const skip = document.querySelector('[data-skip="25"]');
+
+rewind.addEventListener('click', () => {
+    video.currentTime -= 10;
+});
+
+skip.addEventListener('click', () => {
+    video.currentTime += 25;
+});
